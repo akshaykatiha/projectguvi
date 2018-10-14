4515c22b37d6f6869f1109ab27526cc28f7ae558
Akshay Kumar Katiha <40167305+akshaykatiha@users.noreply.github.com>
2018-10-15 00:21:07 +0530

    Create vowel_consonant.c

---
+++ vowel_consonant.c
@@ -0,0 +1,9 @@
+// Check whether a alphabet is vowel or consonant
+#include<stdio.h>
+int main()
+{
+char ch;
+scanf("%c",&ch);//Enter character
+if(ch=='A'||ch=='E'||ch=='I'||ch=='O'||ch=='U'||ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')? printf("Vowel"):printf("Consonant");
+return 0;
+}
